a86fdbeb85558211ce652112e28016e662326591
dani <dani.zaghian@gmail.com>
2021-09-27 10:45:45 -0700

    adds web request to get todos

--- todos.js
+++ todos.js
@@ -1,30 +1,19 @@
-/* global Vue */
+/* global Vue, axios */
 
 var app = new Vue({
   el: "#app",
   data: function() {
     return {
       message: "Hello from JavaScript!",
-      todos: [
-        {
-          id: 1, 
-          title: "walk the dog", 
-          completed: false,
-          userId: 1
-        },
-        {
-          id: 2, 
-          title: "brush my hair", 
-          completed: false,
-          userId: 1
-        },
-        {
-          id: 3, 
-          title: "clean the dishes", 
-          completed: false,
-          userId: 1
-        },
-      ]
+      todos: []
     };
   },
+  methods: {
+    loadTodos: function() {
+      axios.get("https://jsonplaceholder.typicode.com/todos").then(response => {
+        this.todos = response.data;
+        console.log(response.data);
+      });
+    }
+  }
 });
