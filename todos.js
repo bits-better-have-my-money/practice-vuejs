470458af48e7728c70ffc0da1f3dc29d8109a249
dani <dani.zaghian@gmail.com>
2021-09-27 11:41:25 -0700

    adds axios post request with form data

--- todos.js
+++ todos.js
@@ -5,15 +5,30 @@ var app = new Vue({
   data: function() {
     return {
       message: "Hello from JavaScript!",
-      todos: []
+      todos: [],
+      title: ""
     };
   },
+  created: function() {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then(response => {
+      this.todos = response.data;
+      console.log(response.data);
+    });
+  },
   methods: {
-    loadTodos: function() {
-      axios.get("https://jsonplaceholder.typicode.com/todos").then(response => {
-        this.todos = response.data;
+    addTodo: function() {
+      // send a post request using axios to the todo api and then add the new todo object to the todos array
+      var params = {
+        userId: 1,
+        title: this.title,
+        completed: false
+      };
+      axios.post("https://jsonplaceholder.typicode.com/todos", params)
+      .then(response => {
         console.log(response.data);
-      });
+        this.todos.push(response.data);
+        this.title = "";
+      })
     }
   }
 });
